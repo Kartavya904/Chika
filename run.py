6d66639cca7941fb08978e142c23e055b1dc0bc1
bartick <69100224+bartick@users.noreply.github.com>
2021-05-05 16:07:39 +0530

    Updated welcome reaction

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 import time
 from PIL import Image, ImageFont, ImageDraw, ImageOps
 from io import BytesIO
-import aiohttp
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -30,33 +29,29 @@ async def on_member_join(member):
 
     welcome = Image.open("welcome.jpg")
     draw = ImageDraw.Draw(welcome)
-    async with aiohttp.ClientSession() as session:
-        url = member.avatar_url
-        async with session.get(url=str(url)) as response:
-            image_bytes = await response.read()
-    await session.close()
-    pfp = Image.open(BytesIO(image_bytes))
-    pfp = pfp.resize((417,417))
+    pfp = Image.open(BytesIO(await (member.avatar_url).read()))
+    pfp = pfp.resize((215,215))
     mask = Image.open('mask.png').convert('L')
-    mask = mask.resize((417,417))
+    mask = mask.resize((215,215))
     W, H = welcome.size
-    font = ImageFont.truetype("Octagon.ttf", 90)
+    font = ImageFont.truetype("Octagon.ttf", 70)
     w, h = draw.textsize("WELCOME",font)
-    draw.text(((W-w)/2,471),"WELCOME" ,(231, 84, 128),font=font)
-    font = ImageFont.truetype("arial.ttf",55)
+    draw.text(((W-w)/2,228),"WELCOME" ,(231, 84, 128),font=font)
+    font = ImageFont.truetype("arial.ttf",35)
     text = f"{member.name}#{member.discriminator}! Joined the Server!"
     w, h = draw.textsize(text,font)
-    draw.text(((W-w)/2,590),text,(0,0,0),font=font)
-    font = ImageFont.truetype("arial.ttf",45)
+    draw.text(((W-w)/2,308),text,(0,0,0),font=font)
+    font = ImageFont.truetype("arial.ttf",25)
     text = f"We are now at {guild.member_count} Member Strong"
     w, h = draw.textsize(text,font)
-    draw.text(((W-w)/2,670),text,(0,0,0),font=font)
-    welcome.paste(pfp, (619,33), mask)
+    draw.text(((W-w)/2,368),text,(0,0,0),font=font)
+    welcome.paste(pfp, (450,15), mask)
     output_buffer = BytesIO()
     welcome.save(output_buffer,"png")
     output_buffer.seek(0)
 
-    await channel.send(file=discord.File(fp=output_buffer, filename="my_file.png"))
+    content = "Welcome to Heaven's Coffeeshop [AniFarm], {member.mention}! \nPlease read the rules in <#839435259765325824> \nand check out the roles in <#839435259765325824> !\nWe hope you enjoy our service!  💧"
+    await channel.send(content=content, file=discord.File(fp=output_buffer, filename="my_file.png"))
 
 @bot.command(
     name='ping',description='Shows you bots current latency'
